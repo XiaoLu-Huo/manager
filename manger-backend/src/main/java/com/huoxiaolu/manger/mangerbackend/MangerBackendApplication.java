f66282530b28092809ec579a407ee14162a57ded
huoxiaolu <xiaolu.huo@thoughtworks.com>
2023-03-21 20:24:43 +0800

    [feat] init repo

---
+++ manger-backend/src/main/java/com/huoxiaolu/manger/mangerbackend/MangerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.huoxiaolu.manger.mangerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangerBackendApplication.class, args);
+    }
+
+}
---
+++ manger-backend/src/test/java/com/huoxiaolu/manger/mangerbackend/MangerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huoxiaolu.manger.mangerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
